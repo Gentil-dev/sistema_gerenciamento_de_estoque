78bfd44e4652b8c82802f922083d27031ff8c1b2
gentil <gentiln65@gmail.com>
2025-12-06 14:10:20 -0300

    Add a Render no init.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ import os
 db = SQLAlchemy()
 
 def create_app():
-    load_dotenv()
+    if os.getenv("RENDER") is None:
+        load_dotenv()
 
     app = Flask(__name__)
     print(">>> BANCO USADO PELO FLASK:", os.getenv("DATABASE_URL"))
