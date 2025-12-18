52a2ad120c37b8ff104d566fdc2483eec46ba150
gentil <gentiln65@gmail.com>
2025-12-18 16:47:42 -0300

    Ajustes de UX na pág. de estoque

--- app/routes.py
+++ app/routes.py
@@ -41,12 +41,10 @@ def home():
 @bp.route('/gerente', methods=['GET', 'POST'])
 def gerente():
     senha_correta = os.getenv('GERENTE_SENHA')
-    print(">>> GERENTE_SENHA EM PRODUÇÃO:", senha_correta)
-
+ 
     if request.method == 'POST':
         senha_digitada = request.form.get('senha')
-        print(">>> SENHA DIGITADA:", repr(senha_digitada))
-          
+           
         if senha_correta and check_password_hash(senha_correta, senha_digitada):
             session['gerente_logado'] = True
             session.permanent = True
@@ -57,9 +55,7 @@ def gerente():
     return render_template('login.html')
 
     
-
 #painel gerente(protegido)
-
 @bp.route('/painel')
 @gerente_required
 def painel_gerente():
